e4a83730bb2db636a10d8fed4bffeb3813bc6389
Efraim Andrade <efraim.dev@gmail.com>
2019-07-05 22:57:52 -0300

    feat: modulo03
    listagem de prestadores de serviço, migration e model de agendamento, agendamento de serviço, validações de agendamento, listando agendamentos do usuários, adicionando páginação, listando agenda do prestador.

--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -5,9 +5,12 @@ import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
+import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,8 +20,13 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.put('/users', UserController.update);
 
 export default routes;
